c6f3c177fab959c13eb6de13a1426f0f0b2e7542
SMPN20KONSEL <EMAIL_GITHUB_SMPN20KONSEL>
2026-06-02 01:30:24 +0800

    update

---
+++ assets/js/bk.js
@@ -0,0 +1,72 @@
+import { db } from "./firebase.js";
+
+import {
+  doc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+
+/* =========================
+   BASE URL
+========================= */
+
+const BASE_URL =
+window.location.hostname === "localhost"
+  ? "./"
+  : "/WEB.SMPN20KONSEL/";
+
+/* =========================
+   DATA BIMBINGAN KONSELOR
+========================= */
+
+const fotoKepala =
+document.getElementById("fotoKepala");
+
+const nama =
+document.getElementById("nama");
+
+const hp =
+document.getElementById("hp");
+
+const docRef = doc(
+  db,
+  "jabatan",
+  "bimbingan-konselor"
+);
+
+onSnapshot(docRef, (docSnap) => {
+
+  if(docSnap.exists()){
+
+    const data =
+    docSnap.data();
+
+    fotoKepala.src =
+    `${BASE_URL}image/guru/${data.nip}.jpg`;
+
+    fotoKepala.onerror = () => {
+
+      fotoKepala.src =
+      `${BASE_URL}image/default.png`;
+
+    };
+
+    nama.textContent =
+    data.nama || "-";
+
+    hp.textContent =
+    data.nohp || "-";
+
+  }else{
+
+    fotoKepala.src =
+    `${BASE_URL}image/default.png`;
+
+    nama.textContent =
+    "Data tidak ditemukan";
+
+    hp.textContent =
+    "-";
+
+  }
+
+});
