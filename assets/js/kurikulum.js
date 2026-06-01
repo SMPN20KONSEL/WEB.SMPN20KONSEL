84c8bdde319f310b55387b7a5cdd76934ce8de7a
SMPN20KONSEL <EMAIL_GITHUB_SMPN20KONSEL>
2026-06-01 20:50:10 +0800

    update

--- assets/js/kurikulum.js
+++ assets/js/kurikulum.js
@@ -1,44 +1,60 @@
+import { db } from "./firebase.js";
 
-    
-    import { db } from "./firebase.js";
-    
-    import {
-      doc,
-      onSnapshot
-    } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
-    
-    const fotoKepala = document.getElementById("fotoKepala");
-    const nama = document.getElementById("nama");
-    const hp = document.getElementById("hp");
-    
-    const docRef = doc(
-      db,
-      "jabatan",
-      "wakil-kepala-sekolah-bidang-kurikulum"
-    );
-    
-    onSnapshot(docRef, (docSnap) => {
-    
-      if(docSnap.exists()){
-    
-        const data = docSnap.data();
-    
-        fotoKepala.src =
-          data.foto || "image/default.png";
-    
-        nama.textContent =
-          data.nama || "-";
-    
-        hp.textContent =
-          data.nohp || "-";
-    
-      } else {
-    
-        nama.textContent = "Data tidak ditemukan";
-    
-      }
-    
-    });
+import {
+  doc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+
+const BASE_URL =
+window.location.hostname === "localhost"
+  ? "./"
+  : "/WEB.SMPN20KONSEL/";
+
+const fotoKepala =
+document.getElementById("fotoKepala");
+
+const nama =
+document.getElementById("nama");
+
+const hp =
+document.getElementById("hp");
+
+const docRef = doc(
+  db,
+  "jabatan",
+  "wakil-kepala-sekolah-bidang-kurikulum"
+);
+
+onSnapshot(docRef, (docSnap) => {
+
+  if(docSnap.exists()){
+
+    const data = docSnap.data();
+
+    fotoKepala.src =
+    `${BASE_URL}image/guru/${data.nip}.jpg`;
+
+    fotoKepala.onerror = () => {
+
+      fotoKepala.src =
+      `${BASE_URL}image/default.png`;
+
+    };
+
+    nama.textContent =
+    data.nama || "-";
+
+    hp.textContent =
+    data.nohp || "-";
+
+  }else{
+
+    nama.textContent =
+    "Data tidak ditemukan";
+
+  }
+
+});
     
 
 
